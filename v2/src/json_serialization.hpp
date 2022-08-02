7f252f53e1cea29f80652036a7a319257d4abfa7
pink <3213748560@qq.com>
2022-08-01 21:56:13 -0700

    add inline

--- v2/src/json_serialization.hpp
+++ v2/src/json_serialization.hpp
@@ -25,13 +25,13 @@ namespace pson {
 template <typename T, bool is_integer>
 struct FetchJsonDataT
 {
-    void Fetch(pson::Object& o, const std::string& name, T& t) {}
+     inline void Fetch(pson::Object& o, const std::string& name, T& t) {}
 };
 
 template <typename T>
 struct FetchJsonDataT<T, true>
 {
-    void Fetch(pson::Object& o, const std::string& name, T& t) {
+    inline void Fetch(pson::Object& o, const std::string& name, T& t) {
         pson::Number n = o.at(name).as<Number>();
         t = static_cast<T>(n);
     }
@@ -40,7 +40,7 @@ struct FetchJsonDataT<T, true>
 template <typename T>
 struct FetchJsonDataT<T, false>
 {
-    void Fetch(pson::Object& o, const std::string& name, T& t) {
+    inline void Fetch(pson::Object& o, const std::string& name, T& t) {
         t = o.at(name).as<T>();
     }
 };
@@ -49,19 +49,19 @@ struct FetchJsonDataT<T, false>
 template <typename T, bool is_json_type>
 struct enable_if
 {
-    void ObjectInsert(pson::Object& o, std::string name, T t) {}
-    void ObjectFetch(pson::Object& o, const std::string& name, T &t) {}
+    inline void ObjectInsert(pson::Object& o, std::string name, T t) {}
+    inline void ObjectFetch(pson::Object& o, const std::string& name, T &t) {}
 };
 
 template <typename T>
 struct enable_if<T, true>
 {
-    void ObjectInsert(pson::Object& o, std::string name, T t)
+    inline void ObjectInsert(pson::Object& o, std::string name, T t)
     {
         o.insert(std::move(name), t);
     }
 
-    void ObjectFetch(pson::Object& o, const std::string& name, T& t)
+    inline void ObjectFetch(pson::Object& o, const std::string& name, T& t)
     {
        FetchJsonDataT<T, IS_INTEGER(T)>().Fetch(o, name, t);
     }
@@ -70,13 +70,13 @@ struct enable_if<T, true>
 template <typename T>
 struct enable_if<T, false>
 {
-    void ObjectInsert(pson::Object& o, std::string name, T t)
+    inline void ObjectInsert(pson::Object& o, std::string name, T t)
     {
         auto&& json = t.Serialize();
         o.insert(std::move(name), json);
     }
 
-    void ObjectFetch(pson::Object& o, const std::string& name, T& t)
+    inline void ObjectFetch(pson::Object& o, const std::string& name, T& t)
     {
         auto&& v = o.at(name);
         t.DeSerializeFromValue(v);
